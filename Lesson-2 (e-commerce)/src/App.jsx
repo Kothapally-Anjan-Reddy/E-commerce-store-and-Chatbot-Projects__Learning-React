5cd98c66ef23f7532632703a2a63af25ace121ce
Anjan Reddy <kothapallyanjan@gmail.com>
2026-09-09 11:12:01 +0530

    feat: Add Orders and the Tracking Pages along with their styles

--- Lesson-2 (e-commerce)/src/App.jsx
+++ Lesson-2 (e-commerce)/src/App.jsx
@@ -2,7 +2,9 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 import './App.css'
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       // Above the path="/" has a shortcut which is nothing but the index keyword.
       // Here path is the ending url which is given in the website.
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />}/>
+      <Route path="tracking" element={<TrackingPage />}/>
     </Routes>
   );
 }
